7c2821631e445776a724f91607dbaa92630b3fe3
ben196888 <ben196888@gmail.com>
2016-07-17 11:55:55 +0800

    react-bootstrap is awesome

--- app/content.js
+++ app/content.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
-	ButtonToolbar,
-	ButtonGroup,
+	Jumbotron,
 	Button,
 } from 'react-bootstrap';
 
@@ -9,13 +8,19 @@ export default class Content extends React.Component {
 	render() {
 		return (
 			<div className="content">
-				<ButtonToolbar>
-					<ButtonGroup>
-						<Button>Left</Button>
-						<Button>Medium</Button>
-						<Button>Right</Button>
-					</ButtonGroup>
-				</ButtonToolbar>
+  <Jumbotron>
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      This is a simple hero unit,
+      a simple jumbotron-style component for calling extra attention to featured
+      content or information.
+    </p>
+
+    <p><Button bsStyle="primary">Learn more</Button></p>
+
+  </Jumbotron>
 			</div>
 		);
 	}
--- app/header.js
+++ app/header.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import {
-	DropdownButton,
+	Navbar,
+	Nav,
+	NavItem,
+	NavDropdown,
 	MenuItem,
-	ButtonToolbar,
-	Button,
 } from 'react-bootstrap';
 
 export default class Header extends React.Component {
 	render() {
 		return (
 			<div className="header">
-				Header
-				<ButtonToolbar>
-					<Button>Index</Button>
-					<DropdownButton title="Menu">
-						<MenuItem eventKey="1">React</MenuItem>
-						<MenuItem eventKey="2">So Great</MenuItem>
-					</DropdownButton>
-				</ButtonToolbar>
+  <Navbar inverse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 			</div>
 		);
 	}
